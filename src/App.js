c53cbf6f1f469b552f138816f4c51b0af4334421
loch <nigelloch@gmail.com>
2022-03-25 19:40:10 -0500

    created about/nav

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+
+function App() {
+
+  return (
+    <div>
+      <main>
+        <About></About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
