0f4cc23faf54f721f43fd2cf6e3079a9248d6edb
loch <nigelloch@gmail.com>
2022-03-28 21:59:51 -0500

    completed contact rendering/updated other components

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
 
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
     const { name, email, message } = formState;
+    const [errorMessage, setErrorMessage] = useState('');
 
     // JSX
     function handleSubmit(e) {
         e.preventDefault();
         console.log(formState);
       }
-      
+
     return (
         <section>
             <h1>Contact me</h1>
@@ -27,13 +29,32 @@ function ContactForm() {
                     <label htmlFor="message">Message:</label>
                     <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
                 </div>
+                {errorMessage && (
+                <div>
+                    <p className="error-text">{errorMessage}</p>
+                </div>
+                )}
                 <button type="submit">Submit</button>
             </form>
         </section>
     )
-    function handleChange(e) {
-        setFormState({...formState, name: e.target.value })
-      }
+    const handleChange = (e) => {
+        if (e.target.name === 'email') {
+          const isValid = validateEmail(e.target.value);
+          if (!isValid) {
+            setErrorMessage('Your email is invalid.');
+          } else {
+            setErrorMessage('');
+          }
+        } else {
+          if (!e.target.value.length) {
+            setErrorMessage(`${e.target.name} is required.`);
+          } else {
+            setErrorMessage('');
+          }
+        }
+      };
     }
+    
     
     export default ContactForm;
